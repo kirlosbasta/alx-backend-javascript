6420b324c5a0ce0e55646265a5185b9066280ba8
kirlos basta <kirlosbasta@gmail.com>
2024-06-27 02:01:13 +0300

    Add 2-then.js

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,6 +1,8 @@
 export default function handleResponseFromAPI(promise) {
+  const body = { status: 200, body: 'success' };
+
   return promise
-    .then(() => ({ status: 200, body: 'Success' }))
+    .then(() => body)
     .catch((error) => error)
     .finally(() => {
       console.log('Got a response from the API');
