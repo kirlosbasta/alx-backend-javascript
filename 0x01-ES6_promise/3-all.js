8b5ca0f21b7b61215fcb3db3f7e5936283080d8f
kirlos basta <kirlosbasta@gmail.com>
2024-06-27 02:21:42 +0300

    Add 3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  Promise.all([uploadPhoto, createUser])
+  Promise.all([uploadPhoto(), createUser()])
     .then((arr) => {
       console.log(arr[0].body, arr[1].firstName, arr[1].lastName);
     })
