06fc4aaca1443843accfedff937b8237a53b45a5
kirlos basta <kirlosbasta@gmail.com>
2024-06-27 02:15:43 +0300

    Add 3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,1 +1,17 @@
 import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  let body;
+
+  uploadPhoto()
+    .then((obj) => {
+      body = obj.body;
+    })
+    .then(() => createUser())
+    .then((obj) => {
+      console.log(body, obj.firstName, obj.lastName);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
+}
