e32aff5aabc13c3d86c5d2b262ea7c5c0168de5f
kirlos basta <kirlosbasta@gmail.com>
2024-06-27 05:21:25 +0300

    Add 100-await.js

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,15 @@
+import { uploadPhoto, createUser} from './utils';
+
+export default async function asyncUploadUser() {
+  try {
+    const user = await uploadPhoto();
+    const photo = await createUser();
+
+    return { photo, user };
+  } catch (err) {
+    return {
+      photo: null,
+      user: null,
+    };
+  }
+}
