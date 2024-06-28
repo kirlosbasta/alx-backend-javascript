155a70c41e503d31ecd768302b6aabc17ac374a2
kirlos basta <kirlosbasta@gmail.com>
2024-06-28 06:08:49 +0300

    Add  9-hoisting.js

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,12 @@
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  cloneCar() {
+    const SubClass = this.constructor[Symbol.species];
+    return new SubClass(undefined, undefined, undefined);
+  }
+}
