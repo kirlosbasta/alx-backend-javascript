0d97f64f592bf233eedbaffeb38ba4ae620db3e0
kirlos basta <kirlosbasta@gmail.com>
2024-07-04 02:57:58 +0300

    Add task_1 class interface

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -19,3 +19,30 @@ interface printTeacherFunction {
 const printTeacher: printTeacherFunction = (firstName: string, lastName: string): string => {
     return `${firstName[0]}. ${lastName}`;
 }
+
+interface StudentClassInterface {
+    firstName: string;
+    lastName: string;
+
+    workOnHomework(): string;
+    displayName(): string
+}
+
+class StudentClass implements StudentClassInterface {
+    
+    firstName: string;
+    lastName: string;
+
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    workOnHomework(): string {
+        return 'Currently working';
+    }
+
+    displayName(): string {
+        return this.firstName;
+    }
+}
