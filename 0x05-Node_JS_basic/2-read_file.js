974d7b5ed0ace46316f08ece5ebde416453a6e11
kirlos basta <kirlosbasta@gmail.com>
2024-08-20 02:56:01 +0300

    Add 2-read_file.js

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  try {
+    fs.accessSync(path, fs.constants.R_OK);
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+  let content = fs.readFileSync(path, 'utf-8');
+  content = content.trim();
+  const ArrayContent = content.split('\n').slice(1);
+  const fields = {};
+  ArrayContent.forEach((elem, i, arr) => {
+    const student = elem.split(',');
+    const field = student.at(-1);
+    if (Object.hasOwn(fields, field)) {
+      fields[field].count += 1;
+      fields[field].names.push(student[0]);
+    } else {
+      fields[field] = { count: 1, names: [student[0]] };
+    }
+  });
+  console.log(`Number of students: ${ArrayContent.length}`);
+  for (const field of Object.keys(fields)) {
+    console.log(`Number of students in ${field}: ${fields[field].count}. List: ${fields[field].names.join(', ')}`);
+  }
+}
+
+module.exports = countStudents;
