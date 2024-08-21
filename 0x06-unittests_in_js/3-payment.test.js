ec45e947858da4f4e9c557e713c9d1ce8d25ffca
kirlos basta <kirlosbasta@gmail.com>
2024-08-22 01:19:31 +0300

    add tests for 3-payment

---
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -0,0 +1,12 @@
+const sinon = require('sinon');
+const { expect } = require('chai');
+const sendPaymentRequestToApi = require('./3-payment');
+const { Utils } = require('./utils');
+
+describe('sendPaymentRequestToApi', () => {
+  it('should call Utill.calculateNumber', () => {
+    const utilSpy = sinon.spy(Utils, 'calculateNumber');
+    sendPaymentRequestToApi(100, 20);
+    expect(utilSpy.calledOnceWith('SUM', 100, 20)).to.be.true;
+  });
+});
