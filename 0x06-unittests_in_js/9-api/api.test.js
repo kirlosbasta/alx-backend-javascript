7c549531111dd7b1b2bf890444e3e9a2039c2439
kirlos basta <kirlosbasta@gmail.com>
2024-08-22 19:12:59 +0300

    add tests for 9-api

---
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -0,0 +1,27 @@
+const request = require('request');
+const { expect } = require('chai');
+
+describe('integration tests', function () {
+  it('pass with 200 status', function (done) {
+    request.get('http://localhost:7865/cart/12', (err, res, body) => {
+      if (err) {
+        done(err);
+      } else {
+        expect(res.statusCode).to.be.equal(200);
+        expect(body).to.be.equal('Payment methods for cart 12');
+        done();
+      }
+    });
+  });
+
+  it('should fail with 404 status', function (done) {
+    request.get('http://localhost:7865/cart/hello', (err, res, body) => {
+      if (err) {
+        done(err);
+      } else {
+        expect(res.statusCode).to.be.equal(404);
+        done();
+      }
+    });
+  });
+});
