03d6517e953994d6600305f65e763a18b9aba510
kirlos basta <kirlosbasta@gmail.com>
2024-08-22 23:44:12 +0300

    add tests for 9-api

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -16,10 +16,18 @@ describe('integration tests', function () {
 
   it('should fail with 404 status', function (done) {
     request.get('http://localhost:7865/cart/hello', (err, res, body) => {
+      expect(res.statusCode).to.be.equal(404);
+      done();
+    });
+  });
+
+  it('pass with 200 status', function (done) {
+    request.get('http://localhost:7865/', (err, res, body) => {
       if (err) {
         done(err);
       } else {
-        expect(res.statusCode).to.be.equal(404);
+        expect(res.statusCode).to.be.equal(200);
+        expect(body).to.be.equal('Welcome to the payment system');
         done();
       }
     });
